83d12393688a1bfa6829bf0e17e2d6a8b962eddf
Kevoh <40100329+kev254@users.noreply.github.com>
2024-05-24 13:19:41 +0300

    Configured database connection using environment variables

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -5,6 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT // Ensure you add the port if it's specified in your .env file
 });
 
 module.exports = pool;
